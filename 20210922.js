55ae9fd4f2b0ac115a7247da2e5250b4a17e2927
jiadez <jiade33993856@gmail.com>
2021-10-06 14:08:25 +0800

    20201006

---
+++ 20210922.js
@@ -0,0 +1,23 @@
+/*
+const readline = require('readline-sync');
+weight = readline.question('請輸入您的體重(kg)?');
+height = readline.question('請輸入您的身高(cm)?');  
+*/
+/*
+if (...){
+
+}else{
+
+}
+*/
+var weight=50.5;
+var height=160;
+var bmi = weight/((height/100)**2);
+
+console.log("Hello! Your BMI value is "+ bmi);
+
+const readline = require('readline-sync');
+weight = readline.question('請輸入您的體重(kg)?');
+height = readline.question('請輸入您的身高(cm)?'); 
+
+
