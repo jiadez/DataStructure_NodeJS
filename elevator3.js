7aa939be422b9e42a08298bd046a974c1fccf920
jiadez <90750517+jiadez@users.noreply.github.com>
2021-10-13 00:48:00 +0800

    2021012

--- elevator3.js
+++ elevator3.js
@@ -4,21 +4,22 @@ var top = 10;//index value
 var bottom = 0;
 var floorName = ["B3", "B2", "B1", "1", "2", "2M", "3", "5", "6", "7", "8"]
 //1 秀出所有樓層名稱, solution -> 呼叫陣列題工的tostring ( ) 
-//2 用程式建立FloorName 樓層名稱 homework #2 pash() ,pop()s, unshift(),shift()
+//2 用程式建立FloorName 樓層名稱 homework #2 push() ,pop()s, unshift(),shift()
 //3 讓JS　暫停一段時間後續行　ｈｏｍｅｗｏｒｋ#2
 var currentFloor = 5;//index value of floorName
 var targetFloor;//B2->index:1
 
+
 while (true) {//無窮迴圈
     //輸入欲達樓層
-    targetFloor = readline.question('目前在' = floorName[currentFloor] + '樓。請問要去哪一層樓?')
+    targetFloor = readline.question('目前在' + floorName[currentFloor] + '樓。請問要去哪一層樓?')
     //判斷樓層是否合理
     //從floorName.indexof()取出索引值，若為-1代表無此樓層
     targetFloor = targetFloor.toUpperCase();
     targetFloor = floorName.indexOf(targetFloor);//parseInt轉成整數
     // if (isNaN(targetFloor) || targetFloor < bottom || targetFloor > top {
     if (targetFloor < 0) {
-        console.log('請輸入介於' + floorName.toString() + '到' + floorName[top] + '之間的樓層名稱!')
+        console.log('請輸入介於' + floorName.toString() + '之間的樓層名稱!')
         continue;
     }
     if (targetFloor == currentFloor) {
@@ -32,7 +33,7 @@ while (true) {//無窮迴圈
         //改用FOR(初始值;條件判斷;累加減){}
         //while(targetfloor<currentfloor){
         //for(var i=currentfloor;)
-        for (; targetFloor < currentFloor -- ;) {
+        for (; targetFloor < currentFloor ;) {
             //curuenatFloor = currentfloor -1;
             //currentfloor- =1;
             currentFloor--;
